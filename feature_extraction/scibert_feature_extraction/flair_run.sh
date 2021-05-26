fe2f4a631a161ef0fe99eaa3d7240cdd66918970
Ravi Kiran Selvam <sravikiran0606@gmail.com>
2021-05-26 09:52:20 -0700

    Organize the files

--- feature_extraction/scibert_feature_extraction/flair_run.sh
+++
@@ -1,7 +0,0 @@
-# 7: only word-embeddings, 8: use char-embeddings, 9: use flair-embeddings
-# Current Best: 7
-CUDA_VISIBLE_DEVICES=2 python flair_train.py --folder data_processed --idx 9
-
-CUDA_VISIBLE_DEVICES=1 python flair_eval.py --folder score_7 --method chunk \
-| tee logs/flair_eval_7_chunk.log
-
